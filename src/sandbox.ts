787f3a8a97b052e9c77e8fbf6227bd8ab5b6e18c
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2022-09-27 01:39:41 -0300

    08-Function-Basics

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,6 @@
-console.log('testing 1, 2, 3...')
+let f0:Function;
+const f1 = (a:number,b:number)=>{}
+const f2 = (a:number,b?:number)=>{}
+const f3 = (x:string="default")=>{}
+const f4 = (x:string):void=>{console.log(x)}
+const f5 = (x:string):string=>{return x}
